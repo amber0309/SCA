2347d3c455b83436158cc885eb2d693712a0eacf
Seth <ambermit@hotmail.com>
2019-06-02 17:20:39 +0800

    Add files via upload

--- modules/compute_width.m
+++ modules/compute_width.m
@@ -1,6 +1,6 @@
 function md = compute_width(dist_mat)
     % input is the squared distance matrix, i.e. [dist_mat]_{ij} = || x_{i} - x{j} ||^2
-    % output is for exponential kernel with 2*sgm*sgm in denominator
+    % output is the sgm for exponential kernel with 2*sgm*sgm in denominator
 
     half_dist = dist_mat-tril(dist_mat);
     half_dist = reshape(half_dist, size(dist_mat, 1)*size(dist_mat, 2), 1);
